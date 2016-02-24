6f56a3060d4f06852b6284d95d4ae83e40f6dc01
emiliano <chequeremiliano@gmail.com>
2016-02-24 17:05:19 -0300

    commit millon

--- holaMundo/app/helpers/users_helper.rb
+++ holaMundo/app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+def gravatar(email)
+ hash = Digest::MD5.hexdigest(email) 
+ image_tag "http://www.gravatar.com/avatar/#{hash}" 
+end
 end
--- holaMundo/app/models/user.rb
+++ holaMundo/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	#Validaciones de nombre
+ validates :name, :presence => {:message => "Usted debe ingresar un nombre"}, length: {minimum: 2, maximum: 50, :message => "El nombre debe tener entre 2 y 50 caracteres"}
+ #Validaciones de apellido
+ validates :last_name, :presence => {:message => "Usted debe ingresar un apellido"}, length: {minimum: 2, maximum: 50, :message => "El apellido debe tener entre 2 y 50 caracteres"}
+ #Validaciones de email
+ validates :email, :presence => {:message => "Usted debe ingresar un Mail"}, :uniqueness => {:message => "Usted ha ingresado un Mail repetido"}
+
 end
